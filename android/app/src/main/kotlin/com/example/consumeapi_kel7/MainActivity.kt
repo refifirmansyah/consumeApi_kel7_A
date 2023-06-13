d7b5dd0017e00a292ba28f2a34c4d2f291686263
refifirmansyah <refiebezk@gmail.com>
2023-06-13 21:36:27 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/consumeapi_kel7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.consumeapi_kel7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:consumeapi_kel7/utils/app_route.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'ITG Consume API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routeInformationParser: Approutes.goRouter.routeInformationParser,
+      routeInformationProvider: Approutes.goRouter.routeInformationProvider,
+      routerDelegate: Approutes.goRouter.routerDelegate,
+    );
+  }
+}
